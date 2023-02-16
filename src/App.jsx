b8f3a9fb69683b4931a135d617c2c0361529f2fb
ranyu <ranyu@pwrd.com>
2023-02-16 11:12:55 +0800

    code owners add

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,41 @@
+/*
+ * @Author: ryyyyy
+ * @Date: 2023-02-15 19:29:15
+ * @LastEditors: ryyyyy
+ * @LastEditTime: 2023-02-15 19:47:15
+ * @FilePath: /react-vite-test/src/App.jsx
+ * @Description: 
+ * 
+ */
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import './App.css'
 
+import * as Sentry from "@sentry/react";
+import { BrowserTracing } from "@sentry/tracing";
+let release = `react-sentry-github@1.0.0`;
+
+Sentry.init({
+  dsn: "https://1cfce72835ac47d9a6faf4b5821c153c@o1149366.ingest.sentry.io/4504683848794112",
+  integrations: [new BrowserTracing()],
+  tracesSampleRate: 1.0,
+  release
+});
+
+class ValidateError extends Error {
+  constructor(message) {
+    super(message);
+    // this.name = `error: ${message} from ${import.meta.env.VITE_RELEASE}}`;
+    this.name = `error: ${message} from ${release}}`;
+  }
+}
+
 function App() {
   const [count, setCount] = useState(0)
 
+  function handleClickError(err) {
+    throw new ValidateError(err);
+  }
   return (
     <div className="App">
       <div>
@@ -17,7 +48,10 @@ function App() {
       </div>
       <h1>Vite + React</h1>
       <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
+        <button onClick={() => {
+          setCount((count) => count + 1)
+          handleClickError('test error')
+        }}>
           count is {count}
         </button>
         <p>
@@ -31,4 +65,4 @@ function App() {
   )
 }
 
-export default App
+export default Sentry.withProfiler(App)
