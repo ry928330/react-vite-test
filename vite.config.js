4151689746cf5e1d5aae71154d01551ca3eb2938
ranyu <ranyu@pwrd.com>
2023-02-17 18:48:07 +0800

    vite base 修改

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,7 @@
  * @Author: ryyyyy
  * @Date: 2023-02-15 19:29:15
  * @LastEditors: ryyyyy
- * @LastEditTime: 2023-02-16 11:20:44
+ * @LastEditTime: 2023-02-17 18:47:31
  * @FilePath: /react-vite-test/vite.config.js
  * @Description: 
  * 
@@ -12,6 +12,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/dist/',
   plugins: [react()],
   build: {
     sourcemap: true
