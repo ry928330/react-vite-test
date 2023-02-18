cce51e41e9c3cdbec23553e0250ca52fc3b23991
ranyu <ranyu@pwrd.com>
2023-02-18 11:37:31 +0800

    update release

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@
  * @Author: ryyyyy
  * @Date: 2023-02-15 19:29:15
  * @LastEditors: ryyyyy
- * @LastEditTime: 2023-02-18 11:31:37
+ * @LastEditTime: 2023-02-18 11:37:06
  * @FilePath: /react-vite-test/src/App.jsx
  * @Description: 
  * 
@@ -13,7 +13,7 @@ import './App.css'
 
 import * as Sentry from "@sentry/react";
 import { BrowserTracing } from "@sentry/tracing";
-let release = `react-vite-test@v0.23.0`;
+let release = `react-vite-test@v0.24.0`;
 
 Sentry.init({
   dsn: "https://1cfce72835ac47d9a6faf4b5821c153c@o1149366.ingest.sentry.io/4504683848794112",
@@ -26,7 +26,7 @@ class ValidateError extends Error {
   constructor(message) {
     super(message);
     // this.name = `error: ${message} from ${import.meta.env.VITE_RELEASE}}`;
-    this.name = `error: ${message} from ${release}} with number ${Math.random()}`;
+    this.name = `error: ${message} from ${release}} with ${Math.random()}`;
   }
 }
 
