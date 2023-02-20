02bf78a9aa6dfbe87ce3c4bd14b821fa061ad3da
ranyu <ranyu@pwrd.com>
2023-02-20 17:42:23 +0800

    update resease@0.42.0

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,7 @@
  * @Author: ryyyyy
  * @Date: 2023-02-15 19:29:15
  * @LastEditors: ryyyyy
- * @LastEditTime: 2023-02-17 18:47:31
+ * @LastEditTime: 2023-02-20 17:41:39
  * @FilePath: /react-vite-test/vite.config.js
  * @Description: 
  * 
@@ -12,7 +12,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/dist/',
+  // base: '/dist/',
   plugins: [react()],
   build: {
     sourcemap: true
